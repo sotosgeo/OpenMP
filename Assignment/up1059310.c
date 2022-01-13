d406f11a80b0c39690a06b755e27e8dcd7b48265
Sotiris Geo <sotirisgeo99@gmail.com>
2022-01-13 04:33:43 +0200

    Final edition for now

--- Assignment/up1059310.c
+++ Assignment/up1059310.c
@@ -1,6 +1,5 @@
 /*
 This program estimates the value of pi, by using a Monte Carlo area simulation.
-
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,17 +8,17 @@ This program estimates the value of pi, by using a Monte Carlo area simulation.
 #ifdef _OPENMP
 #include <omp.h>
 #endif
-
+#include <limits.h>
 
 
 int main(int argc, char *argv[]) {
 
 
     int thread_count;   
-    long long int total_throws, current_throw, throws_in_circle;
+    unsigned long long int total_throws, current_throw, throws_in_circle;
     double x_pos,y_pos,sum_of_squares;
     thread_count = 4;
-    total_throws = 1e9; 
+    total_throws = 1e6;
 
     //Generating a seed with time
     srand (time (NULL));
@@ -31,9 +30,10 @@ int main(int argc, char *argv[]) {
         total_throws = atol(argv[2]);
     }
 
-
-
+    
     //----Serial Solution----
+    //Uncomment line below to remove serial
+    //*
     throws_in_circle = 0;
     double t0 = omp_get_wtime();
     for (current_throw = 0; current_throw < total_throws; current_throw++) {
@@ -44,7 +44,7 @@ int main(int argc, char *argv[]) {
     }
     double serial_pi_approx = 4.0 * throws_in_circle / ((double) total_throws);
     double t1 = omp_get_wtime();
-
+    //*/
 
 
     //----Parallel Solution----
@@ -55,6 +55,7 @@ int main(int argc, char *argv[]) {
             shared(total_throws) private(x_pos,y_pos, sum_of_squares,current_throw) \
             reduction(+ : throws_in_circle) 
     for (current_throw = 0; current_throw < total_throws; current_throw++) {
+        //Generating 2 random numbers between -1 and 1
         x_pos = (double)rand()/RAND_MAX*2.0-1.0;
         y_pos = (double)rand()/RAND_MAX*2.0-1.0;
         sum_of_squares = x_pos * x_pos + y_pos * y_pos;
@@ -66,12 +67,20 @@ int main(int argc, char *argv[]) {
 
 
     //----Output----
-    printf("Number of threads = %d \n", thread_count);
-    printf("Total darts n = %ld \n", total_throws);
-    printf("Our estimate of pi = %.14f\n", pi_approx);
-    printf("The error from the actual M_PI value is: %.14f\n", M_PI - pi_approx);
+    printf("\nTotal darts n = %llu \n", total_throws);
+    //Uncomment line below to remove serial
+    //*
     
-    printf("time = %.3f sec\n", t1 - t0);
+    printf("//----Serial----//\n");
+    printf("Serial estimate of pi = %.14f\n", serial_pi_approx);
+    printf("The error from the actual M_PI value is: %.14f\n", M_PI - serial_pi_approx);  
+    printf("Serial time = %.3f sec\n\n", t1 - t0);
+    //*/
+    printf("//----Parallel----//\n");
+    printf("Number of threads = %d \n", thread_count);
+    printf("Parallel estimate of pi = %.14f\n", parallel_pi_approx);
+    printf("The error from the actual M_PI value is: %.14f\n", M_PI - parallel_pi_approx);  
+    printf("Parallel time = %.3f sec\n", t3 - t2);
 
     return 0;
 }
